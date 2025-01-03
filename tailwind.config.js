7e5f26d70b5380ecf97fbcbf3e46b6ec2d4ff53c
Del1-SUDO <kurbonovs397@gmail.com>
2025-01-03 22:58:28 +0500

    corrected JS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'sm': '390px',
+        'md': '570px',
+        'lg' : '768px',
+        'xl' : '1024px',
+        '2xl' : '1280px',
+      }
+    },
   },
   plugins: [],
 }
-
